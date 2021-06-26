76eabca77db010402600260be5f58ec35bc5a935
RafaelRossales <rapha2704@gmail.com>
2021-06-26 20:27:24 -0300

    Implementing pagination in mirage API

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,5 +1,6 @@
-import { createServer, Model, Factory } from 'miragejs'
+import { createServer, Model, Factory, Response } from 'miragejs'
 import faker from 'faker'
+import { string } from 'yup/lib/locale'
 
 type User ={
     name:string;
@@ -30,12 +31,30 @@ export function makeServer(){
             })
         },
         seeds(server){ // Cria dados a
-            server.createList('user',10)
+            server.createList('user',200)
         },
         routes(){
             this.namespace = 'api' //  **/api/users/
             this.timing = 750 // Faz com que toda a requizição feita pelo mirage leve ate 750 milisegundos para acontecer
-            this.get('/users'); //ShortHand route (documentacao do mirage)
+            this.get('/users',function(schema,request){
+                const {page = 1, per_page = 10} = request.queryParams
+
+                const total = schema.all('user').length
+
+                // 10 - 20
+                const pageStart = (Number(page) - 1) * Number(per_page);
+
+                const pageEnd = pageStart + Number(per_page);
+
+                const users = this.serialize(schema.all('user')).users.slice(pageStart,pageEnd)
+
+                return new Response(
+                    200,
+                    {'x-total-count':String(total)},
+                    { users }
+                )
+
+            }) //ShortHand route (documentacao do mirage)
             this.post('/users');
 
             this.namespace = '';
